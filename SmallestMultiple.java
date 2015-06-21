306fceb678425127cc5f6ad984203f42fa088f84
arkevines <arkadiuszwilczewski@gmail.com>
2015-06-21 21:04:32 +0200

    Implementacja metody
    Implementacja metody dla uprzednio napisanego testu.

--- SmallestMultiple.java
+++ SmallestMultiple.java
@@ -10,7 +10,27 @@ public class SmallestMultiple {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Poszukiwana liczba to: " + smallestMultiple(20));
+    }
+    
+    public static int smallestMultiple(int upperLimit)
+    {
+        for(int i=2;;i++)
+        {
+            boolean canBeDivided = true;
+            System.out.println("Sprawdzam liczbę: " + i);
+            
+            for(int j=1;j<=upperLimit;++j)
+            {
+                if(i % j != 0)
+                {
+                    canBeDivided = false;
+                    break;
+                }
+            }
+            if(canBeDivided)
+                return i;
+        }
     }
     
 }
