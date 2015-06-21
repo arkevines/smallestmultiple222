6e3f31d2db161fc8eda872d01e88e769f2032617
arkevines <arkadiuszwilczewski@gmail.com>
2015-06-21 21:02:52 +0200

    Rozpoczęcie projektu
    Dodanie testu dla metody wyszukujacej najmniejszą wspólną wielokrotność

---
+++ SmallestMultiple.java
@@ -0,0 +1,16 @@
+package smallestmultiple;
+
+/**
+ *
+ * @author Wilczewski
+ */
+public class SmallestMultiple {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
